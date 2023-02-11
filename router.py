f8634ae3762c108ff510b200f3ff652ffce4d492
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-02-10 22:50:19 -0330

    Some rewrites

--- router.py
+++ router.py
@@ -9,24 +9,16 @@ def load_public_routes(app: Flask):
     # Registered the logo files
     @app.route("/logo192.png")
     def logo192():
-        return send_from_directory(
-            os.path.join(app.root_path, "build"),
-            "logo192.png"
-        )
+        return send_from_directory( os.path.join(app.root_path, "build"), "logo192.png" )
+
     @app.route("/logo512.png")
     def logo512():
-        return send_from_directory(
-            os.path.join(app.root_path, "build"),
-            "logo512.png"
-        )
+        return send_from_directory( os.path.join(app.root_path, "build"), "logo512.png" )
 
     # Register the resume
     @app.route("/resume.pdf")
     def resume():
-        return send_from_directory(
-            os.path.join(app.root_path, "build"),
-            "resume.pdf"
-        )
+        return send_from_directory( os.path.join(app.root_path, "build"), "resume.pdf" )
 
 def load_model_routes(app: Flask):
     """
